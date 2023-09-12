d1dffb01c6499de9da629985149aba09f54dd015
Kristina Salnyk <kristina.salnyk@gmail.com>
2023-09-12 18:29:30 +0200

    bugfix(formatting): add fixes in code formatting

--- src/components/atoms/ShadowBox/ShadowBox.styled.ts
+++ src/components/atoms/ShadowBox/ShadowBox.styled.ts
@@ -1,15 +1,17 @@
 import styled from 'styled-components/native';
 import {Platform} from 'react-native';
 
-export const ShadowBoxStyled = styled.View<{accessibilityRole?: string}>`
+export const ShadowBoxStyled = styled.View<{
+  accessibilityRole?: string;
+}>`
   ${({theme}) =>
     `shadow-color: ${theme.color.black};
     ${
       Platform.OS === 'ios'
-        ? ` 
+        ? `
         shadow-offset: 0;
         shadow-opacity: 0.2;
-        shadow-radius: 4px;
+        shadow-radius: 4px; 
       `
         : `
         elevation: 4;
